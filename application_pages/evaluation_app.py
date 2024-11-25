18b8750a1052ea4744ba960251a4695df2957add
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-11-25 19:46:52 +0530

    update: evaluation app

--- application_pages/evaluation_app.py
+++ application_pages/evaluation_app.py
@@ -8,34 +8,39 @@ weave.init(project_name="guardrails-genie")
 
 st.title(":material/monitoring: Evaluation")
 
-if "start_evaluation" not in st.session_state:
-    st.session_state.start_evaluation = False
-if "ref" not in st.session_state:
-    st.session_state.ref = None
+if "uploaded_file" not in st.session_state:
+    st.session_state.uploaded_file = None
+if "dataset_name" not in st.session_state:
+    st.session_state.dataset_name = ""
+if "visualize_in_app" not in st.session_state:
+    st.session_state.visualize_in_app = False
+if "dataset_ref" not in st.session_state:
+    st.session_state.dataset_ref = None
 
-uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
-dataset_name = st.sidebar.text_input("Dataset name", value="")
+uploaded_file = st.sidebar.file_uploader(
+    "Upload the evaluation dataset as a CSV file", type="csv"
+)
+st.session_state.uploaded_file = uploaded_file
+dataset_name = st.sidebar.text_input("Evaluation dataset name", value="")
+st.session_state.dataset_name = dataset_name
 visualize_in_app = st.sidebar.toggle("Visualize in app", value=False)
+st.session_state.visualize_in_app = visualize_in_app
 
-if uploaded_file is not None:
-    with st.expander("Dataset Preview"):
-        dataframe = pd.read_csv(uploaded_file)
+if st.session_state.uploaded_file is not None and st.session_state.dataset_name != "":
+    with st.expander("Evaluation Dataset Preview"):
+        dataframe = pd.read_csv(st.session_state.uploaded_file)
         data_list = dataframe.to_dict(orient="records")
 
-        if dataset_name != "":
-            dataset = weave.Dataset(name=dataset_name, rows=data_list)
-            st.session_state.ref = weave.publish(dataset)
-            st.write(
-                f"Dataset published at https://wandb.ai/{st.session_state.ref.entity}/{st.session_state.ref.project}/weave/objects/{st.session_state.ref.name}/versions/{st.session_state.ref._digest}"
-            )
+        dataset = weave.Dataset(name=st.session_state.dataset_name, rows=data_list)
+        st.session_state.dataset_ref = weave.publish(dataset)
 
-            if visualize_in_app:
-                st.dataframe(data_list)
-                # dataset = weave.ref("weave:///geekyrakshit/guardrails-genie/object/sample-dataset:RvdLm7KZ5KXFGcXUHWMGoJBWRVmdxiH6VgWu4cpsDHM").get()
+        entity = st.session_state.dataset_ref.entity
+        project = st.session_state.dataset_ref.project
+        dataset_name = st.session_state.dataset_name
+        digest = st.session_state.dataset_ref._digest
+        st.markdown(
+            f"Dataset published to [**Weave**](https://wandb.ai/{entity}/{project}/weave/objects/{dataset_name}/versions/{digest})"
+        )
 
-    run_evaluation_button = st.sidebar.button("Run Evaluation")
-    st.session_state.start_evaluation = run_evaluation_button
-
-    if st.session_state.start_evaluation:
-        with st.expander("Evaluation Results"):
-            st.write("Evaluation results will be displayed here.")
+        if visualize_in_app:
+            st.dataframe(dataframe)
