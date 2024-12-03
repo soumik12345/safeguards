f00294f9e191c680e71ab9abd1c7fee0a00ea178
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-03 14:43:11 +0000

    update: isolate presidio dependencies

--- guardrails_genie/guardrails/entity_recognition/__init__.py
+++ guardrails_genie/guardrails/entity_recognition/__init__.py
@@ -1,5 +1,14 @@
+import warnings
+
 from .llm_judge_entity_recognition_guardrail import RestrictedTermsJudge
-from .presidio_entity_recognition_guardrail import PresidioEntityRecognitionGuardrail
+
+try:
+    from .presidio_entity_recognition_guardrail import PresidioEntityRecognitionGuardrail
+except ImportError:
+    warnings.warn(
+        "Presidio is not installed. You can install it using `pip install -e .[presidio]`"
+    )
+
 from .regex_entity_recognition_guardrail import RegexEntityRecognitionGuardrail
 from .transformers_entity_recognition_guardrail import (
     TransformersEntityRecognitionGuardrail,
