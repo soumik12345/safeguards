e2abb49ee578a22b39615fc879362f4c412d19c4
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-05 04:56:56 +0000

    update: app

--- app.py
+++ app.py
@@ -13,23 +13,23 @@ evaluation_page = st.Page(
     title="Evaluation",
     icon=":material/monitoring:",
 )
-train_classifier_page = st.Page(
-    "application_pages/train_classifier.py",
-    title="Train Classifier",
-    icon=":material/fitness_center:",
-)
-llama_guard_fine_tuning_page = st.Page(
-    "application_pages/llama_guard_fine_tuning.py",
-    title="Fine-Tune LLama Guard",
-    icon=":material/star:",
-)
+# train_classifier_page = st.Page(
+#     "application_pages/train_classifier.py",
+#     title="Train Classifier",
+#     icon=":material/fitness_center:",
+# )
+# llama_guard_fine_tuning_page = st.Page(
+#     "application_pages/llama_guard_fine_tuning.py",
+#     title="Fine-Tune LLama Guard",
+#     icon=":material/star:",
+# )
 page_navigation = st.navigation(
     [
         intro_page,
         chat_page,
         evaluation_page,
-        train_classifier_page,
-        llama_guard_fine_tuning_page,
+        # train_classifier_page,
+        # llama_guard_fine_tuning_page,
     ]
 )
 st.set_page_config(page_title="Guardrails Genie", page_icon=":material/guardian:")
