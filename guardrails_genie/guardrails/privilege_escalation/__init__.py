7c5823ab1cf4e0c9ea7bd9deeed1e4cb6e752423
ayulockin <mein2work@gmail.com>
2024-12-06 16:52:36 +0530

    rename file

--- guardrails_genie/guardrails/privilege_escalation/__init__.py
+++ guardrails_genie/guardrails/privilege_escalation/__init__.py
@@ -1,4 +1,4 @@
-from guardrails_genie.guardrails.privilege_escalation.priv_esc_guardrail import (
+from guardrails_genie.guardrails.privilege_escalation.priv_esc_guardrails import (
     PrivilegeEscalationGuardrail,
 )
 
