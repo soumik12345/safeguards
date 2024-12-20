93b779982805273732060c55f4da0b2ad5def138
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-20 21:00:34 +0530

    fix: imports

--- guardrails_genie/guardrails/injection/__init__.py
+++ guardrails_genie/guardrails/injection/__init__.py
@@ -1,6 +1,6 @@
 from .classifier_guardrail import PromptInjectionClassifierGuardrail
 from .llama_prompt_guardrail import PromptInjectionLlamaGuardrail
-from .survey_guardrail import PromptInjectionLLMGuardrail
+from .llm_guardrail import PromptInjectionLLMGuardrail
 
 __all__ = [
     "PromptInjectionLlamaGuardrail",
