6e1f85a5ffb77f01dd8f7a4a6d6f58129c302ad9
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-20 22:10:05 +0530

    update: tests

--- tests/test_prompt_injection_classifier.py
+++ tests/test_prompt_injection_classifier.py
@@ -1,12 +1,15 @@
 import os
 
 import wandb
+
 from guardrails_genie.guardrails import PromptInjectionClassifierGuardrail
 
 
-def test_prompt_injection_classifier_prompt_guard_86m():
+def test_prompt_injection_classifier_prompt_guard_86m_checkpoint():
+    wandb.login(key=os.getenv("WANDB_API_KEY"), relogin=True)
     guardrail = PromptInjectionClassifierGuardrail(
-        model_name="meta-llama/Prompt-Guard-86M"
+        model_name="meta-llama/Prompt-Guard-86M",
+        checkpoint="wandb://geekyrakshit/guardrails-genie/ruk3f3b4-model:v8",
     )
     result = guardrail.guard("What is the capital of France?")
     assert result["safe"]
@@ -16,11 +19,10 @@ def test_prompt_injection_classifier_prompt_guard_86m():
     assert not result["safe"]
 
 
-def test_prompt_injection_classifier_prompt_guard_86m_checkpoint():
+def test_prompt_injection_classifier_protectai_checkpoint():
     wandb.login(key=os.getenv("WANDB_API_KEY"), relogin=True)
     guardrail = PromptInjectionClassifierGuardrail(
-        model_name="meta-llama/Prompt-Guard-86M",
-        checkpoint="wandb://geekyrakshit/guardrails-genie/ruk3f3b4-model:v8",
+        model_name="ProtectAI/deberta-v3-base-prompt-injection-v2"
     )
     result = guardrail.guard("What is the capital of France?")
     assert result["safe"]
