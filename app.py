049beab8407d9a988cf034bf31b6ccaae322afc3
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-27 18:30:16 +0530

    fix: lint

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from fasthtml.common import FastHTML, FileResponse, serve, Link, Button
-from safeguards.app.tailwind import add_daisy_and_tailwind
+from fasthtml.common import FastHTML, FileResponse, Link, serve
+
 from safeguards.app.components.commons import SafeGuardsNavBar
+from safeguards.app.tailwind import add_daisy_and_tailwind
 
-
-app = FastHTML(
-    hdrs=Link(rel="stylesheet", href="app.css", type="text/css")
-)
+app = FastHTML(hdrs=Link(rel="stylesheet", href="app.css", type="text/css"))
 app.static_route_exts(static_path="public")
 add_daisy_and_tailwind(app)
 route = app.route
+
 
 @app.get("/{fname:path}.{ext:static}")
 def get_static(fname: str, ext: str):
--- safeguards/app/components/commons.py
+++ safeguards/app/components/commons.py
@@ -1,28 +1,27 @@
-from fasthtml.components import Div, A, Ul, Li, Details, Summary
+from fasthtml.components import A, Details, Div, Li, Summary, Ul
 
 
 def SafeGuardsNavBar():
     return Div(
-        Div(
-            A("SafeGuards", cls='btn btn-ghost text-xl'),
-            cls='flex-1'
-        ),
+        Div(A("SafeGuards", cls="btn btn-ghost text-xl"), cls="flex-1"),
         Div(
             Ul(
+                Li(A("Any Chat", href="#")),
+                Li(A("Any Imagine", href="#")),
                 Li(
                     Details(
                         Summary("GuardRails"),
                         Ul(
                             Li(A("Playground", href="#")),
                             Li(A("Evaluation", href="#")),
-                            cls='bg-base-100 rounded-t-none p-2'
-                        )
+                            cls="bg-base-100 rounded-t-none p-2",
+                        ),
                     )
                 ),
                 Li(A("Settings", href="#")),
-                cls='menu menu-horizontal px-1'
+                cls="menu menu-horizontal px-1",
             ),
-            cls='flex-none'
+            cls="flex-none",
         ),
-        cls='navbar bg-base-100'
+        cls="navbar bg-base-100",
     )
