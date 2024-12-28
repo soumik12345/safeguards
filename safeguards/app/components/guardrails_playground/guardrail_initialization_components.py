0fce57f1acf744c0f7d08deb2cf4a26085d60ce7
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-29 00:48:05 +0530

    add: guardrail initialization components for PromptInjectionLLMGuardrail + PromptInjectionClassifierGuardrail

--- safeguards/app/components/guardrails_playground/guardrail_initialization_components.py
+++ safeguards/app/components/guardrails_playground/guardrail_initialization_components.py
@@ -1,4 +1,4 @@
-from fasthtml.common import Div, Input, P
+from fasthtml.common import Div, Form, Input, Option, Select, Span
 
 
 def PromptInjectionLLMGuardrailInitialization():
@@ -8,7 +8,29 @@ def PromptInjectionLLMGuardrailInitialization():
             "PromptInjectionLLMGuardrail: Advance Settings",
             cls="collapse-title text-xl font-medium",
         ),
-        Div(P("PromptInjectionLLMGuardrail"), cls="collapse-content"),
+        Div(
+            Div(
+                Form(
+                    Div(
+                        Span(
+                            "Select LLM for PromptInjectionLLMGuardrail",
+                            cls="label-text",
+                        ),
+                        cls="label",
+                    ),
+                    Select(
+                        Option(""),
+                        Option("gpt-4o"),
+                        Option("gpt-4o-mini"),
+                        cls="select select-bordered",
+                        name="PromptInjectionLLMGuardrail_llm_model",
+                    ),
+                    cls="form-control w-full max-w-xs",
+                ),
+                cls="container mx-auto p-6",
+            ),
+            cls="collapse-content",
+        ),
         cls="collapse collapse-plus bg-base-200",
     )
 
@@ -21,7 +43,24 @@ def PromptInjectionClassifierGuardrailInitialization():
             cls="collapse-title text-xl font-medium",
         ),
         Div(
-            P("PromptInjectionClassifierGuardrail"),
+            Div(
+                Form(
+                    Div(
+                        Span(
+                            "Select LLM for PromptInjectionLLMGuardrail",
+                            cls="label-text",
+                        ),
+                        cls="label",
+                    ),
+                    Input(
+                        type="text",
+                        cls="input input-bordered",
+                        name="PromptInjectionClassifierGuardrail_model_name",
+                    ),
+                    cls="form-control w-full max-w-xs",
+                ),
+                cls="container mx-auto p-6",
+            ),
             cls="collapse-content",
         ),
         cls="collapse collapse-plus bg-base-200",
