14a92dfe92168b1a176586656bf6db841d3511ae
soumik12345 <19soumik.rakshit96@gmail.com>
2025-01-02 21:03:21 +0530

    update: EvaluationTraceParser.register_predict_and_score_calls

--- safeguards/meta_evaluation/trace_parser.py
+++ safeguards/meta_evaluation/trace_parser.py
@@ -1,4 +1,3 @@
-import rich
 import weave
 from rich.progress import track
 
@@ -22,24 +21,12 @@ class EvaluationTraceParser:
                 break
             elif "Evaluation.predict_and_score" in predict_and_score_call._op_name:
                 self.predict_and_score_calls.append(
-                    {
-                        "trace_id": predict_and_score_call.id,
-                        "call_name": self._get_call_name_from_op_name(
-                            predict_and_score_call._op_name
-                        ),
-                        "child_calls": [],
-                    }
+                    self.parse_call(predict_and_score_call)
                 )
-                for child_call in predict_and_score_call.children():
-                    self.predict_and_score_calls[-1]["child_calls"].append(
-                        self.parse_call(child_call)
-                    )
-            rich.print(self.predict_and_score_calls[0]["child_calls"][0])
             break
 
     def parse_call(self, child_call) -> dict:
         call_dict = {
-            "call_id": child_call.id,
             "call_name": self._get_call_name_from_op_name(child_call._op_name),
             "inputs": serialize_input_output_objects(child_call.inputs),
             "outputs": serialize_input_output_objects(child_call.output),
